4f5ec21d14f32bf67beec9b558db2c94b5709f20
PlutoSejin <pluto011030@gmail.com>
2022-04-12 14:27:00 +0900

    update run part and input, output file

--- 1st term project/main.py
+++ 1st term project/main.py
@@ -3,31 +3,29 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import sys
-class LexicalAnalyzer(object):
 
-    #Token Definition
-    VARIALBE = ['int', 'INT', 'char', 'CHAR']
+
+class LexicalAnalyzer(object):
+    # Token Definition
+    VARIABLE = ['int', 'INT', 'char', 'CHAR']
     KEYWORD = ['if', 'IF', 'else', 'ELSE', 'while', 'WHILE', 'return', 'RETURN']
     OPERATOR = ['-', '+', '*', '/']
-    COMPARSION = ['<', '>', '==', '!=', '<=', '>=']
+    COMPARISON = ['<', '>', '==', '!=', '<=', '>=']
     WHITESPACE = ['\t', '\n', ' ']
     BRACE = ['{', '}']
     PAREN = ['(', ')']
     ASSIGN = ['=']
     SEMICOLON = [';']
     COMMA = [',']
+    MERGE = BRACE + PAREN + SEMICOLON + COMMA + OPERATOR[1:] + COMPARISON
 
-    #Letter Definition
-    LETTER = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q,' 'R', 'S', 'T', 'U',
-              'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-              'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    SYMBOL = ['+', '-', '*', '/', '<', '>', '"', '!'] + ASSIGN+BRACE+PAREN+SEMICOLON+COMMA
+    # Letter Definition
+    LETTER = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q,' 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    SYMBOL = ['+', '-', '*', '/', '<', '>', '"', '!'] + ASSIGN + BRACE + PAREN + SEMICOLON + COMMA
     ZERO = ['0']
     NON_ZERO = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     DIGIT = ZERO + NON_ZERO
     STRING = LETTER + DIGIT + SYMBOL + WHITESPACE
-
-    input_file = None
 
     def __init__(self, file):
         self.input_file = file
@@ -40,10 +38,11 @@ class LexicalAnalyzer(object):
         result = ""
         read_all = True
 
-        if len(id)==1:
+        #
+        if len(id) == 1:
             head = id
 
-        while(True):
+        while True:
             if len(id) > 1:
                 if len(buf_id) != 0:
                     head = buf_id[0]
@@ -57,7 +56,7 @@ class LexicalAnalyzer(object):
                     wdT = T[1]
                     result = result + head
                     if read_all:
-                        head = self.input_file.read(1)
+                        head = self.input_file.read(1)   # 1 byte input
 
                 else:
                     return result, False, head
@@ -72,8 +71,11 @@ class LexicalAnalyzer(object):
                 elif head in self.DIGIT:
                     wdT = T[3]
                     result = result + head
-                    if read_all :
+                    if read_all:
                         head = self.input_file.read(1)
+
+                else:
+                    return result, True, head
 
             elif wdT == T[2]:
                 if head in self.LETTER:
@@ -85,8 +87,11 @@ class LexicalAnalyzer(object):
                 elif head in self.DIGIT:
                     wdT = T[3]
                     result = result + head
-                    if read_all :
+                    if read_all:
                         head = self.input_file.read(1)
+
+                else:
+                    return result, True, head
 
             elif wdT == T[3]:
                 if head in self.LETTER:
@@ -98,8 +103,11 @@ class LexicalAnalyzer(object):
                 elif head in self.DIGIT:
                     wdT = T[3]
                     result = result + head
-                    if read_all :
+                    if read_all:
                         head = self.input_file.read(1)
+
+                else:
+                    return result, True, head
 
             if head not in (self.LETTER + self.DIGIT):
                 break
@@ -110,17 +118,17 @@ class LexicalAnalyzer(object):
             return result, False, head
 
     def check_int(self, integer):
-        T= ["T0", "T1", "T2", "T3", "T4", "T5"]
-        wdT = T[0] #working directory T
+        T = ["T0", "T1", "T2", "T3", "T4", "T5"]
+        wdT = T[0]  # working directory T
 
         buf_integer = integer
         result = ""
-        read_all= True
+        read_all = True
 
         if len(integer) == 1:
             head = integer
 
-        while(True):
+        while True:
             if len(integer) > 1:
                 if len(buf_integer) != 0:
                     head = buf_integer[0]
@@ -129,7 +137,10 @@ class LexicalAnalyzer(object):
                 if len(buf_integer) == 0:
                     read_all = True
 
+            print("head: "+head)
+
             if wdT == T[0]:
+                print("check T[0]: " + head+ integer)
                 if head == "-":
                     wdT = T[1]
                     result = result + head
@@ -139,10 +150,10 @@ class LexicalAnalyzer(object):
                 elif head in self.ZERO:
                     wdT = T[2]
                     result = result + head
-                    if read_all :
+                    if read_all:
                         head = self.input_file.read(1)
 
-                elif input in self.NON_ZERO:
+                elif head in self.NON_ZERO:
                     wdT = T[3]
                     result = result + head
                     if read_all:
@@ -177,8 +188,8 @@ class LexicalAnalyzer(object):
                     if read_all:
                         head = self.input_file.read(1)
 
-                else :
-                    return result, False, head
+                else:
+                    return result, True, head
 
             elif wdT == T[4]:
                 if head in self.ZERO:
@@ -194,7 +205,7 @@ class LexicalAnalyzer(object):
                         head = self.input_file.read(1)
 
                 else:
-                    return result, False, head
+                    return result, True, head
 
             elif wdT == T[5]:
                 if head in self.ZERO:
@@ -210,7 +221,7 @@ class LexicalAnalyzer(object):
                         head = self.input_file.read(1)
 
                 else:
-                    return result, False, head
+                    return result, True, head
 
             if head not in self.DIGIT:
                 break
@@ -222,26 +233,26 @@ class LexicalAnalyzer(object):
         else:
             return result, False, head
 
-
     def check_string(self, string):
-        T= ["T0", "T1", "T2", "T3", "T4", "T5"]
-        wdT = T[0] #working directory T
+        T = ["T0", "T1", "T2", "T3", "T4", "T5"]
+        wdT = T[0]  # working directory T
 
         buf_string = string
         result = ""
         read_all = True
 
         if len(string) == 1:
-            input = string
+            head = string
 
-        while (True):
-            if len(string) > 1:
-                if len(buf_string) != 0:
-                    head = buf_string[0]
-                    buf_string = buf_string[1:]
-                    read_all = False
-                if len(buf_string) == 0:
-                    read_all = True
+        while True:
+
+
+            if len(buf_string) != 0:
+                head = buf_string[0]
+                buf_string = buf_string[1:]
+                read_all = False
+            if len(buf_string) == 0:
+                read_all = True
 
             if wdT == T[0]:
                 if head in ['"']:
@@ -278,6 +289,9 @@ class LexicalAnalyzer(object):
                     if read_all:
                         head = self.input_file.read(1)
 
+                else:
+                    return result, False, head
+
             elif wdT == T[2]:
                 return result, True, head
 
@@ -306,6 +320,9 @@ class LexicalAnalyzer(object):
                     if read_all:
                         head = self.input_file.read(1)
 
+                else:
+                    return result, False, head
+
             elif wdT == T[4]:
                 if head in ['"']:
                     wdT = T[2]
@@ -330,6 +347,9 @@ class LexicalAnalyzer(object):
                     result = result + head
                     if read_all:
                         head = self.input_file.read(1)
+
+                else:
+                    return result, False, head
 
             elif wdT == T[5]:
                 if head in ['"']:
@@ -356,7 +376,10 @@ class LexicalAnalyzer(object):
                     if read_all:
                         head = self.input_file.read(1)
 
-            if head not in (self.DIGIT+self.LETTER+[' ','"']):
+                else:
+                    return result, False, head
+
+            if head not in (self.DIGIT + self.LETTER + [' ', '"']):
                 break
 
         if wdT == T[2]:
@@ -364,9 +387,268 @@ class LexicalAnalyzer(object):
         else:
             return result, False, head
 
-
     def run(self):
+        check = True
+        line_num = 1
+        result = ""
+        symbol_table = []
 
+        while True:
+            if check:
+                one = self.input_file.read(1)
+                print("check one: "+one)
+                check = True
+
+            if (one == "") and (result == ""):
+                break
+
+            print("check two: " + one)
+            print("check sub_string: "+result)
+            if (one not in self.STRING) and (result == ""):
+                print("check three: " + one)
+                error_noti = "1Line" + str(line_num) + ": Wrong input format" + one
+                try:
+                    f = open(file_name[:-2]+'_error.out','w')
+                except:
+                    print("Failed to write file")
+                    exit()
+
+                for i in error_noti :
+                    f.writelines(i)
+
+                f.close()
+                print(error_noti)
+                exit()
+
+            if one == '\n':
+                line_num = line_num + 1
+                one = ""
+                check = True
+                continue
+
+            if one in self.WHITESPACE:
+                one = ""
+                check = True
+                continue
+
+            result = result + one
+            one = ""
+            check = True
+
+            if result in self.LETTER:
+                if one == "":
+                    one = self.input_file.read(1)
+                    check = False
+
+                while(one in self.LETTER):
+                    result = result + one
+                    one = self.input_file.read(1)
+
+                if one in self.DIGIT:
+                    check = False
+                    continue
+
+                if result in self.VARIABLE:
+                    symbol_table.append(['variable type', result])
+                    result = ""
+                    continue
+
+                elif result in self.KEYWORD:
+                    symbol_table.append([result, result])
+                    result = ""
+                    continue
+
+            if result in self.ASSIGN:
+                if one == "":
+                    one = self.input_file.read(1)
+                    check = False
+
+                if result + one in self.COMPARISON:
+                    symbol_table.append(['comp', result+one])
+                    result = ""
+                    one = ""
+                    check = True
+                    continue
+
+                else:
+                    symbol_table.append(['assign', result])
+                    result = ""
+                    continue
+
+            if result in ['-']:
+                if (len(symbol_table) != 0) or ('num' in symbol_table[-1]) or ('id' in symbol_table[-1]) or (')' in symbol_table[-1]):
+                    symbol_table.append(['addsub', result])
+                    result = ""
+                    continue
+
+            if result in self.MERGE + ['!']:
+                if result in ['<','>']:
+                    if one == "":
+                        one = self.input_file.read(1)
+                        check = True
+
+                    if one == result:
+                        symbol_table.append(['OPERATOR', result + one])
+                        result = ""
+                        check = True
+                        continue
+
+                    elif result + one in self.COMPARISON:
+                        symbol_table.append(['comp', result + one])
+                        result = ""
+                        check = True
+                        continue
+
+                if result == "!":
+                    if one == "":
+                        one = self.input_file.read(1)
+                        check = True
+
+                    if one == "=":
+                        symbol_table.append(['comp', result + one])
+                        result = ""
+                        check = True
+                        continue
+
+                    else:
+                        error_noti = "2Line" + str(line_num) + ":Wrong input format"
+                        try:
+                            f= open(file_name[:-2]+'_error.out','w')
+                        except :
+                            print("Fail to write file")
+                            exit()
+
+                        for i in error_noti:
+                            f.writelines(i)
+                        f.close()
+                        print(error_noti)
+                        exit()
+
+                if result in self.BRACE:
+                    if result == '{':
+                        symbol_table.append(['lbrace', result])
+                    elif result == '}':
+                        symbol_table.append(['rbrace', result])
+                elif result in self.PAREN:
+                    if result == '(':
+                        symbol_table.append(['lparen', result])
+                    elif result == ')':
+                        symbol_table.append(['rparen', result])
+                elif result in self.SEMICOLON:
+                    symbol_table.append(['semi', result])
+                elif result in self.COMMA:
+                    symbol_table.append(['comma', result])
+                elif result in self.OPERATOR:
+                    if result == '+':
+                        symbol_table.append(['addsub', result])
+                    elif result == '-':
+                        symbol_table.append(['addsub', result])
+                    elif result == '*':
+                        symbol_table.append(['multdiv', result])
+                    elif result == '/':
+                        symbol_table.append(['multdiv', result])
+                    else:
+                        symbol_table.append(['OPERATOR', result])
+                elif result in self.COMPARISON:
+                    symbol_table.append(['comp', result])
+                result = ""
+                continue
+
+            print("digit check:" + result)
+            if result[0] in self.DIGIT + ['-']:
+                result, fact, one = self.check_int(result)
+
+                if fact:
+                    if one ==".":
+                        result = result + one
+                        one = ""
+                        check = True
+                    else:
+                        symbol_table.append(['num', result])
+                        result = ""
+
+                    if result == "":
+                        if one != "":
+                            check = False
+                            continue
+                        else:
+                            check = True
+                            continue
+
+                else:
+                    if one == ".":
+                        result = result + one
+                        one = ""
+                        check = True
+
+                    if result =='-':
+                        result = result + one
+                        one = ""
+                        check = True
+
+                    if result == "":
+                        if one != "":
+                            check = False
+                        else:
+                            check = True
+
+            print("result: "+result)
+            if result[0] in self.LETTER:
+                result, fact, one = self.check_id(result)
+                if fact:
+                    symbol_table.append(['id', result])
+                    result = ""
+                    if one != "":
+                        check = False
+                        continue
+                    else:
+                        check = True
+                        continue
+
+                else:
+                    error_noti = "3Line" + str(line_num) + ": Wrong input format"
+                    # Open file for writing Error
+                    try:
+                        f = open(file_name[:-2]+'_error.out', 'w')
+                    except:
+                        print("Fail to write file")
+                        exit()
+
+                    for i in error_noti:
+                        f.writelines(i)
+                    f.close()
+                    print(error_noti)
+                    exit()
+
+            if result[0] == '"':
+                print("string result check: "+result)
+                result, fact, one = self.check_string(result)
+                if fact:
+                    symbol_table.append(['literal', result])
+                    result = ""
+                    if one != "":
+                        check = False
+                        continue
+                    else:
+                        check = True
+                        continue
+
+                else:
+                    error_noti = "4Line" + str(line_num) + ": Wrong input format"
+                    # Open file for writing Error
+                    try:
+                        f = open(file_name[:-2]+'_error.out', 'w')
+                    except:
+                        print("Fail to write file")
+                        exit()
+
+                    for i in error_noti:
+                        f.writelines(i)
+                    f.close()
+                    print(error_noti)
+                    exit()
+
+        return symbol_table
 
 
 # Press the green button in the gutter to run the script.
@@ -406,5 +688,3 @@ if __name__ == '__main__':
         lexeme = i[1]
         f.writelines(token + ' ' + lexeme + '\n')
     f.close()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ 1st term project/test.c
@@ -0,0 +1,19 @@
+int main(int arc){
+
+    int num1 = 0;
+    int num2 = 0
+    int tmp = 1;
+    char welcom = "hello world";
+
+    if( a > b){
+        tmp = tmp/2;
+        }
+    }
+    else{
+        while(num2<10){
+            num2 = num2 + 0;
+        }
+    }
+
+    return 0;
+}
