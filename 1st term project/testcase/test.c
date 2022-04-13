1be219549745276b8781bc762994beb3e100466d
PlutoSejin <pluto011030@gmail.com>
2022-04-13 16:20:54 +0900

    add comments and fix test.c file

--- 1st term project/testcase/test.c
+++ 1st term project/testcase/test.c
@@ -1,12 +1,14 @@
 int main(int arc){
 
+    char a;
+    int b = (int) a
     int num1= 120;
     int num2 = 1-2;
     int num4 = num4 -2;
     int tmp = -1;
     char welcom = "hello world";
 
-    if(a==b){
+    if(a > b){
         tmp = tmp/2;
         tmp = num1*num2;
     }
