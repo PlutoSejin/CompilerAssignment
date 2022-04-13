8be0a6278bec61d6bc175d15e36aa67332dbe82d
PlutoSejin <pluto011030@gmail.com>
2022-04-13 10:55:31 +0900

    comparison error fix

--- 1st term project/test.c
+++ 1st term project/test.c
@@ -1,17 +1,17 @@
 int main(int arc){
 
-    int num1 = 0;
+    int num1= 120;
     int num2 = 0
-    int tmp = 1;
+    int tmp = -1;
     char welcom = "hello world";
 
-    if( a > b){
+    if(a > b){
         tmp = tmp/2;
-        }
+        tmp = num1*num2
     }
     else{
         while(num2<10){
-            num2 = num2 + 0;
+            num2 = num2-100 ;
         }
     }
 
