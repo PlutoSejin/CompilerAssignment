88b8325ce469fe3a73956923fb01c547da957775
PlutoSejin <pluto011030@gmail.com>
2022-05-31 00:57:07 +0900

    test file normal file

---
+++ 2nd term project/normal_test.c
@@ -0,0 +1,13 @@
+int main(int a, int b) {
+    int k;
+    k= -213;
+    if(a != b) {
+        j = 123;
+    }
+    else {
+        while (k < c) {
+            h = 0;
+        }
+    }
+    return 0;
+}
